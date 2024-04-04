d6946031392e93bc11de70b75ea39e2778dfe5a9
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-04 21:49:42 +0500

    Task no 23

---
+++ index.ts
@@ -0,0 +1,14 @@
+        ////Task # 23///
+
+//Conditional Tests: Write a series of conditional tests. Print a statement describing 
+//each test and your prediction for the results of each test.
+// Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+
+let facultyStudent= "Clear the Test"
+console.log("If Faculty Student clear the test I predict it is true")
+console.log(facultyStudent === "Clear the Test")
+console.log("If Faculty Student clear the test I presdict it is false");
+console.log(facultyStudent !== "Clear the Test")
